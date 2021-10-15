997c9aae4c805e3bac9d44a39cc8f3fb9aa17131
K0v7 <contato.felipesilva2020@hotmail.com>
2021-10-15 17:52:32 -0300

    teste

--- models/image/generate_model_img.py
+++ models/image/generate_model_img.py
@@ -1,6 +1,6 @@
 
 
-from keras.utils import to_categorical, plot_model 
+from keras.utils import to_categorical, plot_model
 from keras.models import load_model
 import sys
 
@@ -8,9 +8,9 @@ model = sys.argv[1]
 model = load_model(model)
 
 model_name = sys.argv[2]
-
-plot_model(model, to_file=model_name+'.png', show_shapes = True)
+plot_model(model, to_file=model_name+'.png', show_shapes=True)
 
 print('[INFO] Summary: ')
 
 model.summary()
+
